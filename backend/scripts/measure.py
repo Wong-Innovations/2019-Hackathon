1e4d5f47915b78a33d28c0f70eeed06240f59f2e
Gage Christensen <grchr4000@gmail.com>
2019-11-09 15:12:29 -0800

    final measure and note.py

--- backend/scripts/measure.py
+++ backend/scripts/measure.py
@@ -8,14 +8,22 @@ class Measure:
     largest_denominator = 0
     for note in notes:
       if note.get_value() > largest_denominator:
-        largest_denominator = note.get_value
+        largest_denominator = note.get_value()
+    
+    print(largest_denominator)
     
     for note in notes:
-      if note.get_value() < largest_denominator:
-        sum += twice_to_this_number(note.get_value(), largest_denominator)
+      note_value = note.get_value()
+      print(note_value)
+
+      if note_value < largest_denominator:
+        note_value = twice_to_this_number(note_value, largest_denominator)
+      
+      sum += note_value
     
-    if sum != largest_denominator:
-      raise NameError("give me a complete measure you fucking donkey")
+    if sum != ((beat_count * largest_denominator) / beat_value):
+      comparison = ((beat_count * largest_denominator) / beat_value)
+      raise NameError("give me a complete measure you fucking donkey", sum, largest_denominator, comparison)
 
     self.notes = notes
     self.beat_value = beat_value
@@ -25,6 +33,8 @@ def twice_to_this_number(num: int, test_num: int, times_called = 1) -> int:
   if num == test_num:
     return times_called
   else:
+    num *= 2
+
     times_called += 1
     return twice_to_this_number(num, test_num, times_called)
 
@@ -36,3 +46,15 @@ def div_by_two_til_one(num: int, times_divided: int = 0) -> int:
 
     times_divided += 1
     return div_by_two_til_one(num, times_divided)
+
+def generate_song(difficulty: int) -> List[Measure]:
+  beat_count = 3
+  beat_value = 4
+
+  first_note = Note("C4", 2, False)
+  second_note = Note("C4", 4, False)
+  return Measure([first_note, second_note], beat_count, beat_value)
+
+
+if __name__ == "__main__":
+  generate_song(0)
--- backend/scripts/trainer.py
+++ backend/scripts/trainer.py
@@ -1,11 +1,38 @@
 import os
 import neat
+from mingus.containers import Note
+
+
+
+ACCURACY = 100
 
 def eval_fitness(genomes, config):
-  pass
+  nets = []
+  ge = []
+  sum_ratios = []
+
+  for _, g in genomes:
+    net = neat.nn.FeedForwardNetwork.create(g, config)
+    nets.append(net)
+    ge.append(g)
+    sum_ratios.append(0)
+  
+  song = generate_song(0)
+  one_beat = ACCURACY
+
 
 def run(config_file: str):
-  pass
+  config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+                              neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                              config_path)
+  
+  population = neat.Population(config)
+  
+  population.add_reporter(neat.StdOutReporter(True))
+  stats = neat.StatisticsReporter()
+  population.add_reporter(stats)
+
+  winner = population.run(eval_fitness, 100)
 
 if __name__ == "__main__":
   local_dir = os.path.dirname(__file__)
