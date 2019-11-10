d65235bf94ff8c996744472c88d3c2cf77188556
Wong-Innovations <dylanwong@nevada.unr.edu>
2019-11-10 11:30:10 -0800

    final

--- frontend/src/views/Learn/Learn.js
+++ frontend/src/views/Learn/Learn.js
@@ -9,17 +9,22 @@ import PianoComponent from './components/Piano';
 import { makeStyles } from '@material-ui/styles';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Fab from '@material-ui/core/Fab';
-import ProMetronome from 'react-pro-metronome'
+import click1 from './click1.mp3';
 
 const useStyles = makeStyles(() => ({
     spacing: {
         height: 40
   }}));
 
+const playClick = () => {
+    // The first beat will have a different sound than the others
+      new Audio(click1).play();
+  }
 
 function Learn(props) {
     const [midiPresent, setMidiPresent] = useState(false);
     const [metronomeOn, setMetronomeOn] = useState(false);
+    const [timer, setTimer] = useState();
 
     WebMidi.enable(function (err) {
 
@@ -43,18 +48,23 @@ function Learn(props) {
             <Fab
                 variant="primary"
                 aria-label="play"
-                onClick={() => {setMetronomeOn(!metronomeOn);}}
+                onClick={() => {
+                    if(metronomeOn) {
+                        // Stop the timer
+                        clearInterval(timer);
+                        setMetronomeOn(false);
+                      } else {
+                        // Start a timer with the current BPM
+                        setTimer(setInterval(playClick, (60 / 80) * 1000));
+                        setMetronomeOn(true);
+                      }
+                    }
+                }
                 style={{position: "fixed", bottom: "20px", right: "20px"}}>
                 <SvgIcon>
                     <path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/>
                 </SvgIcon>
             </Fab>
-            {(metronomeOn)? <ProMetronome
-                bpm={80}
-                subdivision={2}
-                soundEnabled={true}
-                soundPattern="31313131"
-            /> : null}
         </div>
     );
 }
