ad8b2dd614e246b77a9a34e4e785c0a0ea401eb6
Dylan Wong <dylanwong@nevada.unr.edu>
2019-11-10 01:58:21 -0800

    midi input and sound

--- frontend/src/views/Learn/Learn.js
+++ frontend/src/views/Learn/Learn.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../App.css';
 //import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
+import * as WebMidi from "webmidi";
 import SheetMusicDisplay from './components/SheetMusicDisplay';
 import PianoComponent from './components/Piano';
 
 
 function Learn() {
+    const [midiPresent, setMidiPresent] = useState(false);
+
+    WebMidi.enable(function (err) {
+
+        if (err) {
+            console.log("WebMidi could not be enabled.", err);
+        } else {
+            if (WebMidi.inputs.length !== 0) {
+                setMidiPresent(true);
+            }
+            console.log(WebMidi.inputs);
+            console.log(WebMidi.outputs);
+        }
+        
+    });
     return (
         <div>
             <SheetMusicDisplay />
-            <PianoComponent />
+            <PianoComponent midiPresent={midiPresent} />
         </div>
     );
 }
