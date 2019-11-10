f14d734705f6753f1997aa0291de9c2dec8bc539
Wong-Innovations <dylanwong@nevada.unr.edu>
2019-11-10 11:03:07 -0800

    still rushing

--- frontend/src/views/Learn/Learn.js
+++ frontend/src/views/Learn/Learn.js
@@ -9,6 +9,7 @@ import PianoComponent from './components/Piano';
 import { makeStyles } from '@material-ui/styles';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import Fab from '@material-ui/core/Fab';
+import ProMetronome from 'react-pro-metronome'
 
 const useStyles = makeStyles(() => ({
     spacing: {
@@ -18,6 +19,7 @@ const useStyles = makeStyles(() => ({
 
 function Learn(props) {
     const [midiPresent, setMidiPresent] = useState(false);
+    const [metronomeOn, setMetronomeOn] = useState(false);
 
     WebMidi.enable(function (err) {
 
@@ -38,11 +40,21 @@ function Learn(props) {
             <div className={classes.spacing}></div>
             <SheetMusicDisplay location={props.location}/>
             <PianoComponent midiPresent={midiPresent} />
-            <Fab variant="primary" aria-label="play" style={{position: "fixed", bottom: "20px", right: "20px"}}>
+            <Fab
+                variant="primary"
+                aria-label="play"
+                onClick={() => {setMetronomeOn(!metronomeOn);}}
+                style={{position: "fixed", bottom: "20px", right: "20px"}}>
                 <SvgIcon>
                     <path d="M8 5v14l11-7z"/><path d="M0 0h24v24H0z" fill="none"/>
                 </SvgIcon>
             </Fab>
+            {(metronomeOn)? <ProMetronome
+                bpm={80}
+                subdivision={2}
+                soundEnabled={true}
+                soundPattern="31313131"
+            /> : null}
         </div>
     );
 }
